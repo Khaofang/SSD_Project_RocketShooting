c5486835abcb0523b72946f757711399c90a0ece
khaofang <herofactory.th@gmail.com>
2017-05-22 15:23:28 +0700

    edit opponent interface error

--- src/model/Opponent.java
+++ src/model/Opponent.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package model;
 
 public interface Opponent {
@@ -6,12 +5,3 @@ public interface Opponent {
 	public void interrupt();
 	
 }
-=======
-package model;
-
-public interface Opponent {
-
-	public void interrupt();
-	
-}
->>>>>>> 8de83d3f3c43aee1e6d2ab21d3586a2d2b1ab4bd
