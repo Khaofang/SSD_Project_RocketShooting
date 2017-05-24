d1090cda2226ba1ba4bbdd9e9ad5b3006823faac
Nuttapong Rojanavanich <tuter555awesome@gmail.com>
2017-05-24 14:21:26 +0700

    Add Singleton as EnemyBulletPool

--- src/model/EnemyBulletPool.java
+++ src/model/EnemyBulletPool.java
@@ -2,4 +2,22 @@ package model;
 
 public class EnemyBulletPool {
 
+    private EnemyBulletPool instance;
+
+    private List<Bullet> bullets;
+
+    private EnemyBulletPool() {
+        bullets = new ArrayList<Bullet>();
+        for (int i = 0; i < 120; i++) {
+            Bullet b = new Bullet();
+            bullets.add(b);
+        }
+    }
+
+    public static EnemyBulletPool getInstance() {
+        if (instance == NULL)
+            instance = new EnemyBulletPool();
+
+        return instance;
+    }
 }
